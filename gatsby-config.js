fab031a3ca2f310eb657cd9ceb8bb261a20f40fe
David Mullaney <david.mullaney@deptagency.com>
2019-11-14 12:34:29 +0000

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,13 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-source-storyblok',
+      options: {
+        accessToken: 'xBZS3uaDLxI5AP91PO7sLwtt',
+        homeSlug: 'home',
+        version: 'draft'
+      }
+    },
   ],
 }
